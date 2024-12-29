b8bbb5b222142e8291dfd5a30781910ef356d67e
THEPCAGROUP\ysaini <ysaini@pcatg.com>
2024-12-29 18:11:43 -0500

    Add project files.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,31 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDTO> Cities { get; set; }
+
+        public static CitiesDataStore current { get;} = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+
+            Cities = new List<CityDTO>()
+            {
+                new CityDTO()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "Financial hub"
+                },
+                new CityDTO()
+                {
+                    Id = 2,
+                    Name = "Buffalo",
+                    Description = "My City"
+                },
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetOneCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.current.Cities.FirstOrDefault(x => x.Id == id));
+        }
+    }
+}
